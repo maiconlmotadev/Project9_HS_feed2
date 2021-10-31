ff8bfabb57fd1a9ba87d72ff7de34e2ebec56d8e
maiconlmotadev <maiconlmotadev@hotmail.com>
2021-10-31 15:28:11 +0000

    Lançamento de projeto no github

---
+++ Project7/Petition.swift
@@ -0,0 +1,14 @@
+//
+//  Petition.swift
+//  Project7
+//
+//  Created by Maicon Mota on 24/10/2021.
+//
+
+import Foundation
+
+struct Petition: Codable {
+    var title: String
+    var body: String
+    var signatureCount: Int
+}
---
+++ Project7/Petitions.swift
@@ -0,0 +1,12 @@
+//
+//  Petitions.swift
+//  Project7
+//
+//  Created by Maicon Mota on 24/10/2021.
+//
+
+import Foundation
+
+struct Petitions: Codable {
+    var results: [Petition]
+}
